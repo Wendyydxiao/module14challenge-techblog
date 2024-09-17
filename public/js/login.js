899f9eb82d18ea892b37c21556dae9fb801cac99
Wendyydxiao <169132887+Wendyydxiao@users.noreply.github.com>
2024-09-17 21:43:39 +1000

    fix signup issue

--- public/js/login.js
+++ public/js/login.js
@@ -25,11 +25,11 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#usrename-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/login', {
+  if (username && password) {
+    const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
